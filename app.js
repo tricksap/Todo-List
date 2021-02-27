f20cdd938896839da92158c089027e9325b335b7
Patrick <tricksap@gmail.com>
2021-02-27 21:56:11 +0800

    First Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+const app = express()
+app.set('view engine', 'ejs');
+
+app.get("/",function (req,res){
+    var today = new Date();
+
+    var options={
+        weekday: "long",
+        day:"numeric",  
+        month:"long"    
+    };
+
+    var day = today.toLocaleDateString("en-US",options);
+    res.render("list",{kindOfDay:day});
+})
+
+
+app.listen(3000,function(){
+    console.log("Server Running port 3000.....")
+})
