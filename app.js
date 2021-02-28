de9495cd9566c83bf6068f91e27973aea143eede
Patrick <tricksap@gmail.com>
2021-02-28 09:26:14 +0800

    add item list

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express")
-
+const bodyParser = require("body-parser")
 const app = express()
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
+
+items=[]
 
 app.get("/",function (req,res){
     var today = new Date();
@@ -13,8 +16,14 @@ app.get("/",function (req,res){
     };
 
     var day = today.toLocaleDateString("en-US",options);
-    res.render("list",{kindOfDay:day});
+    res.render("list",{kindOfDay:day, newListItems:items});
 })
+
+app.post("/",function(req,res){
+    var item = req.body.newItem
+    items.push(item)   
+    res.redirect("/")
+});
 
 
 app.listen(3000,function(){
