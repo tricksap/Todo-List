d368e7348f23890accb2cde8837d9f97373fcc6c
Patrick <tricksap@gmail.com>
2021-02-28 09:59:43 +0800

    add style

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-items=[]
+items=["sad","sdads"]
 
 app.get("/",function (req,res){
     var today = new Date();
