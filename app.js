12ff8bc0eb715f49b15598c27d6f1c6421939fd8
Patrick <tricksap@gmail.com>
2021-03-18 19:28:03 +0800

    try

--- app.js
+++ app.js
@@ -128,6 +128,11 @@ app.post("/delete", function (req, res) {
   }
 });
 
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
   console.log("Server Running port 3000.....");
 });
