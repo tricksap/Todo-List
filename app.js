1b1ea9bfa81c2cc47d1ec8495318fba207c13afc
Patrick <tricksap@gmail.com>
2021-03-18 18:45:14 +0800

    add env

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
 const _ = require("lodash");
+require("dotenv").config();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/list", {
+mongoose.connect(process.env.MONGOLAB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
